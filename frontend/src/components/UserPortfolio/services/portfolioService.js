c43bb3d21bdc4c05d375d63cf9778e2b6a0d6cea
phphint <124293060+phphint@users.noreply.github.com>
2023-12-14 11:23:16 -0500

    Update portfolioService.js

--- frontend/src/components/UserPortfolio/services/portfolioService.js
+++ frontend/src/components/UserPortfolio/services/portfolioService.js
@@ -1,7 +1,8 @@
 // portfolioService.js
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5001/api/';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5001/api/';
+
 
 export const fetchPortfolioData = async (identifier, token = null) => {
   try {
