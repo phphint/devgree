ea4726d17f0577284990fe1c2130adf4e0292565
phphint <124293060+phphint@users.noreply.github.com>
2023-12-14 11:23:05 -0500

    Update PortfolioHeader.jsx

--- frontend/src/components/UserPortfolio/PortfolioHeader.jsx
+++ frontend/src/components/UserPortfolio/PortfolioHeader.jsx
@@ -5,11 +5,6 @@ const UserInfo = () => {
   const email = useSelector((state) => state.userPortfolio.data.email);
   const profile = useSelector((state) => state.userPortfolio.data.profile);
 
-  const userPortfolio = useSelector((state) => state.userPortfolio.data);
-  console.log('User Portfolio State:', userPortfolio);
-
-
-  // Safely access languages with optional chaining and nullish coalescing
   const languages = useSelector((state) => 
     state.userPortfolio.resume?.languages?.map(lang => `${lang.language} (${lang.proficiency})`).join(', ') ?? 'Languages not available'
   );
@@ -18,35 +13,33 @@ const UserInfo = () => {
   const phone = profile?.phone || 'Phone not available';
 
   return (
-    <div className="container my-4">
-      <div className="row">
+    <div className="container shadow-wrapper ">
+      <div class=" "> 
+      <div className="row navbar-height bg-white text-dark">
         {/* Location */}
-        <div className="col-md-3 text-center">
-          <i className="fas fa-map-marker-alt fa-2x"></i>
-          <h5 className="mt-2">Location</h5>
-          <p>{location}</p>
+        <div className="col-md-3 d-flex align-items-center">
+          <i className="fas fa-map-marker-alt me-2"></i>
+          <span>{location}</span>
         </div>
 
         {/* Email */}
-        <div className="col-md-3 text-center">
-          <i className="fas fa-envelope fa-2x"></i>
-          <h5 className="mt-2">Email</h5>
-          <p>{email}</p>
+        <div className="col-md-3 d-flex align-items-center">
+          <i className="fas fa-envelope me-2"></i>
+          <span>{email}</span>
         </div>
 
         {/* Phone */}
-        <div className="col-md-3 text-center">
-          <i className="fas fa-phone fa-2x"></i>
-          <h5 className="mt-2">Phone</h5>
-          <p>{phone}</p>
+        <div className="col-md-3 d-flex align-items-center">
+          <i className="fas fa-phone me-2"></i>
+          <span>{phone}</span>
         </div>
 
         {/* Languages */}
-        <div className="col-md-3 text-center">
-          <i className="fas fa-language fa-2x"></i>
-          <h5 className="mt-2">Languages</h5>
-          <p>{languages}</p>
+        <div className="col-md-3 d-flex align-items-center">
+          <i className="fas fa-language me-2"></i>
+          <span>{languages}</span>
         </div>
+      </div>
       </div>
     </div>
   );
