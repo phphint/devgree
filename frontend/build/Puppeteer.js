1fdafa697b175a6b327c7ebc4797ab404e7fd736
phphint <124293060+phphint@users.noreply.github.com>
2023-12-24 10:01:07 -0500

    addd static rendered pages for bot support to main website front end non app pages

---
+++ frontend/build/Puppeteer.js
@@ -0,0 +1,28 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const path = require('path');
+
+async function renderPage(url) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url, { waitUntil: 'networkidle2' });
+  const content = await page.content();
+  await browser.close();
+  return content;
+}
+
+async function saveRenderedPages(baseURL, routes) {
+    for (const route of routes) {
+      // Check if the route is for the home page
+      const pageURL = route === 'home' ? baseURL : `${baseURL}/${route}`;
+      const content = await renderPage(pageURL);
+      const fileName = route === 'home' ? 'index.html' : `${route}.html`;
+      fs.writeFileSync(path.join(__dirname, 'rendered', fileName), content);
+    }
+  }
+  
+  const baseURL = 'https://www.devgree.com'; // Your app's URL
+  const routes = ['home', 'about', 'privacy', 'features', 'terms']; // 'home' represents the root route '/'
+  
+
+saveRenderedPages(baseURL, routes).then(() => console.log('Pages rendered successfully'));
--- frontend/server.js
+++ frontend/server.js
@@ -1,17 +1,27 @@
+// server.js
 const express = require('express');
 const path = require('path');
 const compression = require('compression');
-const app = express();
+const botUserAgents = require('./botUserAgents'); // import the list of bot user agents
 
-// Use compression middleware to compress text responses
+const app = express();
 app.use(compression());
 
-// Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'build')));
 
-// Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/build/index.html'));
+  const userAgent = req.headers['user-agent'].toLowerCase();
+  const isBot = botUserAgents.some(botAgent => userAgent.includes(botAgent));
+
+  if (isBot) {
+    // Serve static HTML for bots
+    // Example: If the URL is '/about', serve 'about.html' from the 'rendered' directory
+    const route = req.path === '/' ? 'home' : req.path.substring(1);
+    res.sendFile(path.join(__dirname, 'rendered', `${route}.html`));
+  } else {
+    // Serve the standard React app for regular users
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  }
 });
 
 const port = process.env.PORT || 3000;
