7f15d8450fd9abf7eafb7a8fc8c0ecd2894752cd
phphint <124293060+phphint@users.noreply.github.com>
2023-12-26 18:36:05 -0500

    Update Puppeteer.js

--- frontend/Puppeteer.js
+++ frontend/Puppeteer.js
@@ -3,7 +3,12 @@ const fs = require('fs');
 const path = require('path');
 
 async function renderPage(url) {
-	const browser = await puppeteer.launch({ headless: "new" });
+  const browser = await puppeteer.launch({
+    headless: "new",
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
+  
+  
   
   const page = await browser.newPage();
   await page.goto(url, { waitUntil: 'networkidle2' });
