65b9a85332636b4d608e746428becb8add31dfce
phphint <124293060+phphint@users.noreply.github.com>
2023-12-28 10:56:18 -0500

    Update Puppeteer.js

--- frontend/Puppeteer.js
+++ frontend/Puppeteer.js
@@ -27,7 +27,7 @@ async function saveRenderedPages(baseURL, routes) {
     }
   }
   
-  const baseURL = 'https://www.devgree.com:3000'; // Your app's URL
+  const baseURL = 'http://www.devgree.com:3000'; // Your app's URL
   const routes = ['home', 'about', 'privacy', 'features', 'terms']; // 'home' represents the root route '/'
   
 
