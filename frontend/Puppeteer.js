3bd4953d1bbdfd471b6991393610b1acd0e21936
phphint <124293060+phphint@users.noreply.github.com>
2023-12-28 11:13:54 -0500

    Update Puppeteer.js

--- frontend/Puppeteer.js
+++ frontend/Puppeteer.js
@@ -4,31 +4,41 @@ const path = require('path');
 
 async function renderPage(url) {
   const browser = await puppeteer.launch({
-    headless: "new",
+    headless: true,
     args: ['--no-sandbox', '--disable-setuid-sandbox']
   });
-  
-  
-  
+
   const page = await browser.newPage();
-  await page.goto(url, { waitUntil: 'networkidle2' });
-  const content = await page.content();
-  await browser.close();
-  return content;
+  try {
+    console.log(`Navigating to ${url}`);
+    await page.goto(url, { waitUntil: 'networkidle2', timeout: 60000 }); // 60 seconds
+    console.log(`Page loaded: ${url}`);
+    const content = await page.content();
+    await browser.close();
+    return content;
+  } catch (error) {
+    console.error(`Error while navigating to ${url}: ${error.message}`);
+    await browser.close();
+    throw error; // Rethrow the error to handle it in the calling function
+  }
 }
 
 async function saveRenderedPages(baseURL, routes) {
-    for (const route of routes) {
-      // Check if the route is for the home page
-      const pageURL = route === 'home' ? baseURL : `${baseURL}/${route}`;
+  for (const route of routes) {
+    const pageURL = route === 'home' ? baseURL : `${baseURL}/${route}`;
+    try {
       const content = await renderPage(pageURL);
       const fileName = route === 'home' ? 'index.html' : `${route}.html`;
       fs.writeFileSync(path.join(__dirname, 'rendered', fileName), content);
+      console.log(`Page saved: ${fileName}`);
+    } catch (error) {
+      console.error(`Failed to save page ${pageURL}: ${error.message}`);
+      // Optionally, handle the error further or continue to the next iteration
     }
   }
-  
-  const baseURL = 'https://www.devgree.com'; // Your app's URL
-  const routes = ['home', 'about', 'privacy', 'features', 'terms']; // 'home' represents the root route '/'
-  
+}
+
+const baseURL = 'https://www.devgree.com';
+const routes = ['home', 'about', 'privacy', 'features', 'terms'];
 
 saveRenderedPages(baseURL, routes).then(() => console.log('Pages rendered successfully'));
